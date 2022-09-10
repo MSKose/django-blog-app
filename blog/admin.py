82e30c98ce884fb0a12e7f26728f0eaeda91e45b
MSKose <saitmustafa1998@gmail.com>
2022-09-10 10:00:41 +0200

    comments model added, working on the front-end

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
-# admin.site.register(Post)
+admin.site.register(Post)
+admin.site.register(Comment)
 
 # class PostAdmin(admin.ModelAdmin):
 #     model = Post
--- blog/models.py
+++ blog/models.py
@@ -29,3 +29,11 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse("post-detail", kwargs={"pk": self.pk})
     
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments") 
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.post.title} - {self.name}"
